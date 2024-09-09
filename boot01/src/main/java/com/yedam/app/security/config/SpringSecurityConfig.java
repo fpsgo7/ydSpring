645ce84483a9fd1d2fc36a881da89996510a38c2
fpsgo7 <lkj4769@naver.com>
2024-09-09 17:51:48 +0900

    스프링 시큐리티 시작하기
    pom.xml : 스프링 시큐리티를 위한 dependency를 추가한다.
    
    SpringSecurityConfig : 스프링 시큐리티의 설정들을 작성하였다.
    1. 비밀번호 암호화를 위한 passwordEncoder 빈 추가
    2. 메모리상 인증 정보 등록 inMemoryUserDetailsService
    	회원을 메모리상으로 추가한다. (생성된 꼐정으로 로그인 이 가능하다.)
    3. 스프링의 핵심 설정인 필터체인에 내용을 추가하였다.
    	1. URI 에 권한 확인 추가하기
    	2. 로그인 과 로그아웃에 대한 설정을 추가하였다.
    		둘다 성공시에대한 uri 추가하였다.
    
    SampleController : 페이지를 열어주는 컨트롤러를 작성하였다. uri 에대한 권한 확인이 설정되있어
    인증 없이 권한 없이 특정 요청을 할 수 없다.
    권한 없이 요청할경우 403 오류가 나타난다.

---
+++ boot01/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.yedam.app.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity// 시큐리티 관련 
+public class SpringSecurityConfig {
+	
+	@Bean// 비밀번호 암호화
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean // 메모리상 인증정보 등록 => 테스트 전용
+	InMemoryUserDetailsManager inMemoryUserDetailsService() {
+		// UserDetails 시큐리티 의 VO같은 존재
+		// user1 회원 추가
+		UserDetails user = User.builder()// builder 패턴 사용
+				.username("user1")
+				// 암호화된 비밀번호값을 대입한다.
+				.password(passwordEncoder().encode("1234"))
+				.roles("USER")// ROLE_USER <-- 강제로 ROLE_ 가 붙여진다.
+				//.authorities("ROLE_USER") // ROLE_가 강제로 붙여지지 않는다는 차이가 있다.
+				.build();
+		//  admin 회원 추가
+		UserDetails admin = User.builder()// builder 패턴 사용
+				.username("admin1")
+				// 암호화된 비밀번호값을 대입한다.
+				.password(passwordEncoder().encode("1234"))
+				.roles("ADMIN")// ROLE_USER <-- 강제로 ROLE_ 가 붙여진다.
+				.build();
+		// 매개값을 여러개 주어 여러 회원을 만들수 있다.
+		return new InMemoryUserDetailsManager(user,admin);
+	}
+	
+	// 스프링 설정 필터 체인
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// 적용될 Security 설정
+		//  => URI에 적용될 권한
+		http
+			.authorizeHttpRequests(authorize
+				-> authorize
+				.requestMatchers("/","/all").permitAll() // permitAll() 모든 권한 허가
+				// 권한이 없다면 403 오류가 발생한다.
+				.requestMatchers("/user/**").hasAnyRole("USER","ADMIN")// 여러 권한조건을 넣고 싶을때 사용
+				//.requestMatchers("/user/**").hasRole("USER")// ROLE_ 은 앞에 알아서 붙여준다.
+				.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")// 규칙명을 풀네임으로 작성해야한다.
+				.anyRequest().authenticated() // 인증 된 유저에게 나머지에 대하여 허가
+		);
+		
+		// 로그인 성공시 와, 로그아웃 성공시 설정
+		http
+			.formLogin(formlogin ->  formlogin.defaultSuccessUrl("/all"))
+			.logout(logout -> logout.logoutSuccessUrl("/all"));
+		
+		return http.build();
+	}
+	
+}
