b4168890ae38a369f2a854cd0ef24a4d608b64db
fpsgo7 <lkj4769@naver.com>
2024-09-11 18:26:11 +0900

    스프링부트 게시판 프로젝트  : 뷰작성하기
    뷰를 작성하여 컨트롤러가 열어야할 페이지가 완성되었다.
    
    BoardVO.java : boardInsert.html 과 관련하여 주석을 추가하였다.
    input 태그의 type="date" 의 날짜포맷과 자바 의 Data 날짜 포멧은 서로 다르기에 입력하는 곳에서 맞춰줄지
    서버에서 맞춰줄지에 대한 주석이다.

--- boot_board/src/main/java/com/yedam/app/board/service/BoardVO.java
+++ boot_board/src/main/java/com/yedam/app/board/service/BoardVO.java
@@ -2,6 +2,8 @@ package com.yedam.app.board.service;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -10,6 +12,7 @@ public class BoardVO {
 	private String title;	// 제목
 	private String contents;// 내용
 	private String writer;	// 작성자
+	// @DateTimeFormat(pattern="yyy-MM-dd") // 해당 어노테이션이 있다면 date 타입(0000-00-00 포맷)으로 값을 받을 수 있다.
 	private Date regdate;	// 작성일
 	private Date updatedate;// 수정일
 	private String image;	// 첨부 이미지
